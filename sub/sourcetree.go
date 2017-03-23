ef3ccc878a6187c62dafe8695388dfe160c41ae4
Takahiro Yamazaki <yamazaki@nvs-one.com>
2017-03-23 15:05:51 +0900

    add main line

--- sub/sourcetree.go
+++ sub/sourcetree.go
@@ -1,9 +1,26 @@
 package sourcetree
 
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+)
+
 func CheckArg(args []string) bool {
 	count := len(args)
 	if count < 2 || count > 3 {
 		return false
 	}
 	return true
+}
+
+func CallWinMerge(args []string) {
+	prevDir, _ := filepath.Abs(".")
+	os.Chdir(fmt.Sprintf("cd %s\n", args[1]))
+	command := fmt.Sprintf("git windiff %s %s\n", args[2], args[3])
+	fmt.Printf(command)
+	exec.Command(command)
+	os.Chdir(prevDir)
+
 }
